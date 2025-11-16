256511e27996eae236a04929b84e0058acdb3760
RafaMango <rafaelabascunan2005@gmail.com>
2025-11-15 23:47:07 -0300

    Delete src/main/java/puppy/code directory

--- src/main/java/puppy/code/Destructible.java
+++
@@ -1,8 +0,0 @@
-package puppy.code;
-
-// Lo que todas las cosas que pueden morir necesitan
-public interface Destructible {
-    int getVida();
-    void recibirDanio(int danio);
-    boolean estaDestruido();
-}
--- src/main/java/puppy/code/Hostil.java
+++
@@ -1,6 +0,0 @@
-package puppy.code;
-
-// Lo que todas las cosas que tienen un rol de enemigo/hostil necesitan
-public interface Hostil {
-    int getPuntosPorDestruccion();
-}
